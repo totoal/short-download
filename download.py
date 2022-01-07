66d14506235d12c01fbc1115cd34eceb121b79d2
totoal <ttalbertott@gmail.com>
2022-01-07 11:00:13 +0100

    First commit

---
+++ download.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import pytube
+
+def download_vid(url, folder):
+    you = pytube.YouTube(url)
+
+    print(f'Downloading: {you.title}')
+
+    vid = you.streams.get_highest_resolution()
+
+    vid.download(folder)
+
+if __name__ == '__main__':
+    playlist_url = 'https://youtube.com/playlist?list=PLMj2crhs4VJS5lmbg0BIfHoljLSDZKfrW'
+
+    p = pytube.Playlist(playlist_url)
+
+    try:
+        folder = sys.argv[1]
+    except:
+        folder = './'
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    for url in p.video_urls:
+        download_vid(url, folder)
